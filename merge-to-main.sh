3f78bfab9c2e49b87fb5cd1c04ec3ceec1ddaad5
Oksana KRANGACH <oksana.krangach@external.danone.com>
2024-04-05 00:00:40 +0300

    test

--- merge-to-main.sh
+++ merge-to-main.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+git checkout release
+git pull
 git checkout main
+git pull
 git merge release
 git commit -m "New release"
 git push origin
--- merge-to-release.sh
+++ merge-to-release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+git checkout develop
+git pull
 git checkout release
+git pull
 git merge develop
 git commit -m "Release update"
 git push origin
