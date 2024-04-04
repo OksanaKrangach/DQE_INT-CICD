d41645ae248f00e7a435ee86ab03a401fa2ffd3c
Oksana KRANGACH <oksana.krangach@external.danone.com>
2024-04-05 01:05:45 +0300

    test

--- merge-to-release.sh
+++ merge-to-release.sh
@@ -9,6 +9,9 @@ git checkout release
 git pull
 git merge develop
 git commit -m "Release update"
+echo "====================================================================="
+git status
+echo "====================================================================="
 git push
 
 git checkout feature1
