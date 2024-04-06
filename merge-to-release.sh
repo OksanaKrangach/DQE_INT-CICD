91ad61ed257c1d4c1c30b191ae9b5c1a1613b3c2
Oksana KRANGACH <oksana.krangach@external.danone.com>
2024-04-06 12:43:33 +0300

    test

--- merge-to-release.sh
+++ merge-to-release.sh
@@ -3,22 +3,18 @@
 #git config --global user.email "oksana_krangach@epam.com"
 #git config --global user.name "Oksana Krangach"
 
-echo "====================================================================="
-echo "START"
-pwd
-echo "User name: ${GIT_USERNAME}, pass: ${GIT_PASSWORD}"
-echo "====================================================================="
-git status
-echo "====================================================================="
+echo "#####################################################################"
+echo "START merge_to_release"
+echo "---------------------------------------------------------------------"
+
 git checkout develop
 git pull
 git checkout release
 git pull
 git merge develop
 git commit -m "Release update"
-echo "====================================================================="
-git status
-echo "====================================================================="
-#git push
-
 git checkout feature1
+
+echo "---------------------------------------------------------------------"
+echo "FINISH  merge_to_release"
+echo "#####################################################################"
